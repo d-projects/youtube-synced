05b228497f64b0d6e72babc71d2bcd5b8cd92f9b
d-projects <60767346+d-projects@users.noreply.github.com>
2020-10-11 21:29:03 -0700

    Change port to support website on heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const { render } = require('ejs');
 const express = require('express');
 const app = express();
+const port = process.env.PORT;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('working');
 });
 
