35a6566ca02d806d165bde65b6281832890fa548
d-projects <60767346+d-projects@users.noreply.github.com>
2020-10-24 12:26:12 -0700

    Move config to separate file

--- app.js
+++ app.js
@@ -8,23 +8,22 @@ const session = require('express-session');
 const socketio = require('socket.io');
 const server = http.createServer(app);
 const mongoose = require('mongoose');
+const Room = require('./models/room.js');
+const config = require('./config.js');
 const io = socketio(server);
 const port = process.env.PORT || 3000;
-const config = require('./settings/config.js');
 
-
+app.use(session({
+    secret: 'random string',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-
-const config = {
-    db_user: 'dulhan',
-    db_password: 'cbtjTNU626tSBkGV',
-    db_cluster: 'blogapp',
-    db_name: 'youtube-party-db',
-}
 
 const dbURI = `mongodb+srv://${config.db_user}:${config.db_password}@${config.db_cluster}.ffgxc.mongodb.net/${config.db_name}`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -46,22 +45,16 @@ let count = 0;
 
 io.on('connection', socket => {
 
-    console.log('Webscoket connection working');
+    console.log('Webscoket connection working!');
     const user = "user";
 
-    if (!session.master){
-        //session.users = 0;
-        session.master = socket;
-    }
-    //session.users += 1;
-    // console.log(session.users)
-    // console.log(session.embedID);
+
+
     console.log(session.room);
     socket.join(session.room);
     socket.emit('join', {
         joinMessage: "Welcome",
-        videoID: session.embedID,
-        master: (session.master == socket) ? true : false
+        master: (session.isMaster) ? true : false
     });
     socket.broadcast.to(session.room).emit('join', {
         joinMessage: `${user} has joined the chat`
@@ -77,9 +70,34 @@ io.on('connection', socket => {
     });
 
     socket.on('sync', () => {
-        if (session.master != socket) {
-            session.master.emit('getTime');
+        if (!session.isMaster) {
+            session.masterSocket.emit('getTime');
             session.temp = socket;
+        }
+        if (session.isMaster){
+            Room.findOne({room: session.room})
+            .then( result => {
+                result.masterSocket = socket;
+                result.save()
+                .then( res => {
+                    console.log('yess')
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            })
+            .catch( err => {
+                console.log(err);
+            })
+            session.masterSocket = socket;
+        } else {
+            Room.findOne({room: session.room})
+            .then( result => {
+                session.masterSocket = result.masterSocket;
+            })
+            .catch( err => {
+                console.log(err);
+            })
         }
     });
 
@@ -88,18 +106,17 @@ io.on('connection', socket => {
         session.embedID = null;
         socket.broadcast.emit('join', `${user} has left the chat`)
         session.open = false;
+        Blog.deleteOne({room: session.room})
+        .then( result => {
+            console.log('Yay')
+        })
+        .catch (err => {
+            console.log(err);
+        })
         
     });
 
 });
-
-server.listen(port, () => {
-    console.log('working');
-});
-
-// app.use( (req, res, next) => ){
-//     if (session.open) {}
-// }
 
 /**
  * Used by Fetch API to get youtube video ID
@@ -126,19 +143,35 @@ app.get('/', (req, res) => {
  */
 
 app.post('/watch', (req, res) => {
-    const url = parse(req.body.url, true);
-    if (session.embedID == undefined || session.embedID == null){      
-        session.embedID = url.query.v;
-    }
-    const room = Math.floor(Math.random() * 100000);
-    session.room = room;
-    res.render('watch', {title: 'Watch', room: room});
+    const url = parse(req.body.url, true);   
+    session.embedID = url.query.v;
+    const roomID = Math.floor(Math.random() * 100000);
+    session.room = roomID;
+    const room = new Room({room: roomID, videoID: session.embedID});
+    session.isMaster = true;
+    room.save()
+    .then( result => {
+        console.log('saved');
+    })
+    .catch( err => {
+        console.log(err);
+    })
+    res.render('watch', {title: 'Watch', room: session.room});
 });
 
 app.post('/join', (req, res) => {
     const room = req.body.room;
     session.room = room;
-    res.render('watch', {title: 'Watch', room: room});
+    Room.findOne({room: room})
+    .then( result => {
+        session.embedID = result.videoID;
+        console.log('hereee')
+    })
+    .catch( err => {
+        console.log(err);
+    })
+    session.isMaster = false;
+    res.render('watch', {title: 'Watch', room: session.room});
 });
 
 app.use( (req, res) => {
---
+++ models/room.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    room: {
+        type: Number,
+        required: true
+    },
+    videoID: {
+        type: String,
+        required: true
+    },
+    masterSocket: {
+        type: String,
+        required: false
+    }
+});
+
+const Room = mongoose.model('room', roomSchema);
+
+module.exports = Room;
